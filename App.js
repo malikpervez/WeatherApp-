736168b96a0af3f5f917b167d83c67c60903d2c2
dutchkillsg <mip12793@gmail.com>
2017-12-18 15:37:10 -0500

    some issues rendering forecast, api test works

--- App.js
+++ App.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#666666',
   },
   welcome: {
     fontSize: 20,

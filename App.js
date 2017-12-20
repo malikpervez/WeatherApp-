f07bdb9983bafdaa9d09a2e59bbcc0863ecb1e42
dutchkillsg <mip12793@gmail.com>
2017-12-20 17:19:30 -0500

    changed some styling but having image background issues

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput
+  TextInput,
+  Image
 } from 'react-native';
 import Forecast from './components/Forecast';
 import Weather from './components/Weather'
@@ -38,41 +39,78 @@ class WeatherApp extends Component {
     }
   return (
     <View style = {styles.container}>
-      <Text style = {styles.welcome}>
-        Please Input {this.state.zip}
-      </Text>
-      <TextInput
-        style = {styles.input}
-        onSubmitEditing={this._handleTextChange}
-      />
-      {content}
+      <Image 
+        source={require('./images/tree.jpg')}
+        resizeMode='cover'
+        style={styles.backdrop}
+      >
+       </Image>
+      <View style={styles.overlay}>
+        <View style={styles.row}>
+          <Text style = {styles.mainText}>
+          Current Weather For
+          </Text>
+          <View style={styles.zipContainer}>
+            <TextInput
+              style={[styles.zipcode, styles.mainText]}
+              onSubmitEditing={event => this._handleTextChange(event)}
+            />
+          </View>
+        </View>
+          {content}
+        </View>
+     
     </View>
+  
     )
   }
 }
 
+const baseFontSize = 16;
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#666666',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  input: {
-    fontSize: 20,
-    borderWidth: 2,
-    padding: 2,
-    height: 40,
-    width: 100,
-    textAlign: 'center'
-  },
-});
-
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    alignItems: "center",
+     paddingTop: 30
+    
+    }, 
+    backdrop: { 
+      flex: 1, 
+      flexDirection: "column" 
+    }, 
+    overlay: { 
+      paddingTop: 5,
+      backgroundColor: "#000000", 
+      opacity: 0.5, 
+      flexDirection: "column", 
+      alignItems: "center" 
+    
+      }, 
+    row: { 
+      flexDirection: "row", 
+      flexWrap: "nowrap", 
+      alignItems: "flex-start",
+      padding: 30 
+      }, 
+    zipContainer: {
+       height: baseFontSize + 10, 
+       borderBottomColor: "#DDDDDD",
+       borderBottomWidth: 1, 
+       marginLeft: 5,
+       marginTop: 3 
+      
+      }, 
+    zipCode: { 
+      flex: 1, 
+      flexBasis: 1, 
+      width: 50, 
+      height: baseFontSize,
+    },
+    mainText: { 
+      fontSize: baseFontSize,
+      color: "#FFFFFF" 
+    }
+  });
+    
 export default WeatherApp;
